1e123cf7bd0413d1045f402033cb87d8b8ac80d2
Adriana <svecaadriana05@gmail.com>
2026-09-23 16:58:56 +0100

    Finished the about us section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from 'react'
 import Nav from "./Sections/Nav.jsx";
 import './App.css'
 import Hero from './components/Hero.jsx'
+import AboutUs from './components/AboutUs.jsx'
 
 function App() {
   return (
     <>
       <Nav/>
       <Hero />
+      <AboutUs/>
     </>
   )
 }
