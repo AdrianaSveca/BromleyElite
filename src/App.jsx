338425698e7f4b9e4454ef7198356ac0d995a989
Cousality <165505726+Cousality@users.noreply.github.com>
2026-09-23 21:31:19 +0100

    Created Files

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { useState } from 'react'
 import './App.css'
 
 import Nav from "./components/Nav.jsx";
-import Hero from './components/Hero.jsx'
+import Hero from './components/Hero.jsx';
+import Flooring from './components/Flooring.jsx';
 import AboutUs from './components/AboutUs.jsx'
 import WhyChooseUs from './components/WhyChooseUs.jsx';
 
@@ -12,6 +13,7 @@ function App() {
     <>
       <Nav/>
       <Hero />
+      <Flooring />
       <AboutUs/>
       <WhyChooseUs/>
     </>
---
+++ src/components/Flooring.jsx
@@ -0,0 +1,11 @@
+import '../styling/flooring.css';
+
+function Flooring() {
+  return (
+    <div>
+        <h1>xyz</h1>
+    </div>
+  );
+}
+
+export default Flooring;
